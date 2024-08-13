948710ade8f0779134eaabbdd081094adb75162b
nel11326 <lenor11326@hotmail.com>
2024-08-13 14:15:34 +0800

    feat(update): rendering the examples conditionally

--- src/components/main/main.component.jsx
+++ src/components/main/main.component.jsx
@@ -6,10 +6,24 @@ import TabButton from "../TabButton/tabButton.component.jsx";
 import "./main.component.css";
 
 function Main() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState("");
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -38,13 +52,7 @@ function Main() {
           <TabButton onSelect={() => handleSelect("props")} children="Props" />
           <TabButton onSelect={() => handleSelect("state")} children="State" />
         </menu>
-        <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic].title[0]}</h3>
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>{EXAMPLES[selectedTopic].code}</code>
-          </pre>
-        </div>
+        {tabContent}
       </section>
     </main>
   );
